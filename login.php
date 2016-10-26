b58006746ed473c35528f286ecad1548828e2341
sngjss <sunga.jss@gmail.com>
2016-10-26 13:05:05 -0400

    Added login page.

---
+++ login.php
@@ -0,0 +1,15 @@
+<?php
+
+	include 'includes/header.php'; 
+
+	
+?>
+
+<!-- Body -->
+		<div class="container" id="content">
+			<div class="row">
+			 </div>
+		</div>
+<!-- /Body -->
+		
+<?php include 'includes/footer.php'; ?>
