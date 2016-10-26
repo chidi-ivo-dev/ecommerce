f2e7a0febbd2c123d6ed40aa55bd6e77ef52c172
sngjss <sunga.jss@gmail.com>
2016-10-26 13:12:30 -0400

    Setup test for login.

--- login.php
+++ login.php
@@ -2,12 +2,22 @@
 
 	include 'includes/header.php'; 
 
-	
 ?>
 
 <!-- Body -->
 		<div class="container" id="content">
 			<div class="row">
+<?php
+				if (isset($_SESSION['logged_in'])) {
+
+					print ("<h1>You are logged in. You'lll be re-directed to the homepage in 5 seconds</h1>");
+					header("Refresh:5;url=home.php");
+
+				} else {
+
+					print ("<h1>You are not logged in.</h1>");
+				}
+?>
 			 </div>
 		</div>
 <!-- /Body -->
