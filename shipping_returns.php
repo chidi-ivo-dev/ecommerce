b9bd932bbb9e1730fe3903fe8bfab60189359f0d
pparsard <pparsard@users.noreply.github.com>
2016-11-16 02:06:45 -0500

    Add files via upload
    Added code for the Shipping and Returns page

---
+++ shipping_returns.php
@@ -0,0 +1,68 @@
+<?php 
+	include 'includes/header.php'; 
+?>
+
+<!-- Body -->
+
+		
+
+<?php
+				if ($_SESSION['logged_in'] == true) {
+					print ("logged in session is active <br/>");
+				} else {
+					print ("logged in session error. </br>");
+				}
+?>			
+
+		<div class="container" id="content">
+
+				<div class="row title-section">
+						<div class="twelve columns">
+								<h2>Shipping and Returns</h2>
+						</div>
+				</div>
+
+				<div class="row">
+						<div class="row u-cf u-full-width product-card">
+  								<div class="twelve columns">
+  										<h3>Business Policies</h3> 	
+										<p>Our Shipping rates are based on the distance between our furniture warehouse and the shipping address provided by the customer. For a shipping distance of 0-30 miles, there is a $70 delivery fee. For a shipping distance of 31 or more miles, please call us for pricing.</p>
+  								</div>		  	
+						</div>
+						<div class="row u-cf u-full-width product-card">
+  								<div class="twelve columns">
+  										<h3>Shipping Policy and Charges</h3> 	
+										<p>For now, we only ship within the US. Customers will receive a confirmation email for their order containing a tracking number from one of the major shipping providers (USPS, UPS, FedEx, or DHL). Orders over $100 will qualify for Free Shipping. Shipping fees will also vary based on the country where your package is being delivered. If any items mailed to the same U.S. shipping address are ordered on the same day of the week, those items will all be packaged into the same box. If you have any questions regarding missing items from your order, please contact us immediately via phone or email so that we can work towards resolving your problem.<br><br>Delivery of furniture items is expected to take between 2-4 weeks for arrival.</p>
+  								</div>		  	
+						</div>
+						<div class="row u-cf u-full-width product-card">
+  								<div class="twelve columns">
+  										<h3>Tax Policy</h3> 	
+										<p>Depending on where the order is being shipped, sales tax will be charged on the subtotal, shipping, and/or handling at the applicable county rate. Tax rates between countries will vary</p>
+  								</div>		  	
+						</div>
+						<div class="row u-cf u-full-width product-card">
+  								<div class="twelve columns">
+  										<h3>Return Policy</h3> 	
+										<p>Eligible returns will be accepted within 90 days from the item’s date of purchase with a valid sales receipt.</p>
+  								</div>		  	
+						</div>
+						<div class="row u-cf u-full-width product-card">
+  								<div class="twelve columns">
+  										<h3>Privacy Policy</h3> 	
+										<p>Chairman’s Bestfriend will never disclose your personal information to third-party corporations. Also, you may feel free to opt-out of receiving email offers from us at any time.</p>
+  								</div>		  	
+						</div>
+						<div class="row u-cf u-full-width product-card">
+  								<div class="twelve columns">
+  										<h3>Security Statement</h3> 	
+										<p>We take important security measures to ensure comfort among our customers as they use our website. Your personal information and online transactions are sent through our secure servers using Secure Socket Layering (SSL) encryption, protecting them from unauthorized access. Moreover, our customer service center uses a private, secure network.</p>
+  								</div>		  	
+						</div>
+				</div>
+
+		</div>
+
+<!-- /Body -->		
+		
+<?php include 'includes/footer.php'; ?>
