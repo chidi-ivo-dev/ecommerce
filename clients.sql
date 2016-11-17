0e8ab4df319d4e82978bac9cf8f1e6d9f805440c
judmy1 <judmy1@gmail.com>
2016-11-16 20:32:16 -0500

    sql username update

--- clients.sql
+++ clients.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 15, 2016 at 12:05 AM
+-- Generation Time: Nov 17, 2016 at 02:07 AM
 -- Server version: 10.1.9-MariaDB
 -- PHP Version: 5.6.15
 
@@ -28,17 +28,26 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `clients` (
   `client_id` int(9) NOT NULL,
-  `first_name` varchar(30) CHARACTER SET latin1 NOT NULL,
-  `last_name` varchar(30) CHARACTER SET latin1 NOT NULL,
-  `phone` varchar(15) CHARACTER SET latin1 NOT NULL,
-  `address` varchar(30) CHARACTER SET latin1 NOT NULL,
-  `apt_suite` varchar(15) CHARACTER SET latin1 NOT NULL,
-  `city` varchar(20) CHARACTER SET latin1 NOT NULL,
-  `state` varchar(2) CHARACTER SET latin1 NOT NULL,
-  `zipcode` varchar(10) CHARACTER SET latin1 NOT NULL,
-  `email` varchar(40) CHARACTER SET latin1 DEFAULT NULL,
-  `password` varchar(12) CHARACTER SET latin1 DEFAULT NULL
+  `first_name` varchar(30) NOT NULL,
+  `last_name` varchar(30) NOT NULL,
+  `phone` varchar(15) NOT NULL,
+  `address` varchar(30) NOT NULL,
+  `apt_suite` varchar(15) NOT NULL,
+  `city` varchar(20) NOT NULL,
+  `state` varchar(2) NOT NULL,
+  `zipcode` varchar(10) NOT NULL,
+  `email` varchar(40) DEFAULT NULL,
+  `password` varchar(40) DEFAULT NULL,
+  `user_level` varchar(10) DEFAULT NULL COMMENT 'guest or admin',
+  `username` varchar(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Clients data';
+
+--
+-- Dumping data for table `clients`
+--
+
+INSERT INTO `clients` (`client_id`, `first_name`, `last_name`, `phone`, `address`, `apt_suite`, `city`, `state`, `zipcode`, `email`, `password`, `user_level`, `username`) VALUES
+(8, 'David', 'Mayorca', '4074147897', '458 Central Ave', '879', 'Orlando', 'FL', '32746', 'mayorcad@gmail.com', 'd033e22ae348aeb5660fc2140aec35850c4da997', 'admin', 'admin');
 
 --
 -- Indexes for dumped tables
@@ -48,7 +57,10 @@ CREATE TABLE `clients` (
 -- Indexes for table `clients`
 --
 ALTER TABLE `clients`
-  ADD PRIMARY KEY (`client_id`);
+  ADD PRIMARY KEY (`client_id`),
+  ADD UNIQUE KEY `username_2` (`username`),
+  ADD KEY `email` (`email`),
+  ADD KEY `username` (`username`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -58,7 +70,7 @@ ALTER TABLE `clients`
 -- AUTO_INCREMENT for table `clients`
 --
 ALTER TABLE `clients`
-  MODIFY `client_id` int(9) NOT NULL AUTO_INCREMENT;
+  MODIFY `client_id` int(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
